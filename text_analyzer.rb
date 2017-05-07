dd677e321f80a509800d0211d788791da397a9ec
Muhammad Affan Khan <maffan@folio3.com>
2017-05-07 14:58:22 +0500

    added char count without spaces

--- text_analyzer.rb
+++ text_analyzer.rb
@@ -1,10 +1,25 @@
 puts('Welcome to the Text Analyzer Program...!')
 
+# Features:
+character_count_with_space = 0
+character_count_without_space = 0
+word_count = 0
+line_count = 0
+
 #text file path
 file_path = 'text_analysis_files/simple_paragraph.txt'
 
 #reading files line-wise
 text_file = File.readlines(file_path)
 
-#print the no. of lines
-puts("#{text_file.size}")
+# calculating
+line_count = text_file.size
+
+text_file.each {|line|
+  word_count += line.split(' ').size
+  character_count_with_space += line.gsub("\n",'').size
+  character_count_without_space += line.gsub("\n",'').gsub(' ','').size
+}
+
+puts("***Summary***\nTotal lines in file: #{line_count}\nTotal characters(with spaces): #{character_count_with_space}\nTotal Characters(without spaces): #{character_count_without_space}\nWord Count: #{word_count}")
+
