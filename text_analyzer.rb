806f28fee408d7c77c83364b1e612e955c3246ce
Muhammad Affan Khan <maffan@folio3.com>
2017-05-07 15:14:54 +0500

    refactor

--- text_analyzer.rb
+++ text_analyzer.rb
@@ -4,7 +4,6 @@ puts('Welcome to the Text Analyzer Program...!')
 character_count_with_space = 0
 character_count_without_space = 0
 word_count = 0
-line_count = 0
 
 #text file path
 file_path = 'text_analysis_files/simple_paragraph.txt'
@@ -12,8 +11,6 @@ file_path = 'text_analysis_files/simple_paragraph.txt'
 #reading files line-wise
 text_file = File.readlines(file_path)
 
-# calculating
-line_count = text_file.size
 
 text_file.each {|line|
   word_count += line.split(' ').size
@@ -21,5 +18,5 @@ text_file.each {|line|
   character_count_without_space += line.gsub("\n",'').gsub(' ','').size
 }
 
-puts("***Summary***\nTotal lines in file: #{line_count}\nTotal characters(with spaces): #{character_count_with_space}\nTotal Characters(without spaces): #{character_count_without_space}\nWord Count: #{word_count}")
+puts("***Summary***\nTotal lines in file: #{text_file.size}\nTotal characters(with spaces): #{character_count_with_space}\nTotal Characters(without spaces): #{character_count_without_space}\nWord Count: #{word_count}")
 
